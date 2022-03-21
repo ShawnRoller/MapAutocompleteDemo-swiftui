6af3cd2741668efa8f073f53f34350f80f27df64
ShawnRoller <offensivelybad@yahoo.com>
2022-03-21 17:35:05 -0400

    Initial commit

--- MapAutocompleteDemo/ContentView.swift
+++ MapAutocompleteDemo/ContentView.swift
@@ -4,13 +4,88 @@
 //
 //  Created by Shawn Roller on 3/21/22.
 //
+// from https://stackoverflow.com/a/67131376/6199526
 
 import SwiftUI
+import MapKit
+import CoreLocation
 
 struct ContentView: View {
+    @StateObject private var mapSearch = MapSearch()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Address", text: $mapSearch.searchTerm)
+                }
+                Section {
+                    ForEach(mapSearch.locationResults, id: \.self) { location in
+                        NavigationLink(destination: Detail(locationResult: location)) {
+                            VStack(alignment: .leading) {
+                                Text(location.title)
+                                Text(location.subtitle)
+                                    .font(.system(.caption))
+                            }
+                        }
+                    }
+                }
+            }.navigationTitle(Text("Address search"))
+        }
+    }
+}
+
+class DetailViewModel : ObservableObject {
+    @Published var isLoading = true
+    @Published private var coordinate : CLLocationCoordinate2D?
+    @Published var region: MKCoordinateRegion = MKCoordinateRegion()
+    
+    var coordinateForMap : CLLocationCoordinate2D {
+        coordinate ?? CLLocationCoordinate2D()
+    }
+    
+    func reconcileLocation(location: MKLocalSearchCompletion) {
+        let searchRequest = MKLocalSearch.Request(completion: location)
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { (response, error) in
+            if error == nil, let coordinate = response?.mapItems.first?.placemark.coordinate {
+                self.coordinate = coordinate
+                self.region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
+                self.isLoading = false
+            }
+        }
+    }
+    
+    func clear() {
+        isLoading = true
+    }
+}
+
+struct Detail : View {
+    var locationResult : MKLocalSearchCompletion
+    @StateObject private var viewModel = DetailViewModel()
+    
+    struct Marker: Identifiable {
+        let id = UUID()
+        var location: MapMarker
+    }
+    
+    var body: some View {
+        Group {
+            if viewModel.isLoading {
+                Text("Loading...")
+            } else {
+                Map(coordinateRegion: $viewModel.region,
+                    annotationItems: [Marker(location: MapMarker(coordinate: viewModel.coordinateForMap))]) { (marker) in
+                    marker.location
+                }
+            }
+        }.onAppear {
+            viewModel.reconcileLocation(location: locationResult)
+        }.onDisappear {
+            viewModel.clear()
+        }
+        .navigationTitle(Text(locationResult.title))
     }
 }
 
