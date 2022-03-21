9fd3601defaedf516c2457358bcd205d0d75b0ce
ShawnRoller <offensivelybad@yahoo.com>
2022-03-21 16:28:45 -0400

    Initial Commit

---
+++ MapAutocompleteDemo/MapAutocompleteDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAutocompleteDemoApp.swift
+//  MapAutocompleteDemo
+//
+//  Created by Shawn Roller on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MapAutocompleteDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
